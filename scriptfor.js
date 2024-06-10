5f12ebff5a775653bf11d489037ad6d6ae316dc7
Kell22-mkt <kellyckaroline@gmail.com>
2024-06-10 06:59:07 -0300

    via cep

---
+++ scriptfor.js
@@ -0,0 +1,23 @@
+document.getElementById('cep').addEventListener('blur', function() {
+    const cep = this.value.replace(/\D/g, '');
+    if (cep.length === 8) {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                if (!('erro' in data)) {
+                    document.getElementById('street').value = data.logradouro;
+                    document.getElementById('neighborhood').value = data.bairro;
+                    document.getElementById('city').value = data.localidade;
+                    document.getElementById('state').value = data.uf;
+                } else {
+                    alert('CEP não encontrado!');
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao buscar o CEP:', error);
+                alert('Erro ao buscar o CEP!');
+            });
+    } else {
+        alert('CEP inválido!');
+    }
+});
